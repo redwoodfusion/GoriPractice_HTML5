aee4c7752a551074b096f0512959af308c969d76
b1011233 Yugo Akagi <b1011233@fun.ac.jp>
2013-05-23 19:35:33 +0900

    start making analogclock

---
+++ HTML5/analogclock.js
@@ -0,0 +1,120 @@
+var displayPanel;
+var controlPanel;
+
+
+window.onload = appInit;
+
+
+function appInit(){
+
+	displayPanel = document.getElementById("displayPanel");
+	tick();	
+	
+	initColorPicker();
+	
+	var filePicker = document.getElementById("filePicker");
+	var fileButton = document.getElementById("fileButton");
+	filePicker.addEventListener("change", setBackground, false);
+	fileButton.addEventListener("click", function(){filePicker.click();}, false);
+
+	loadSettings();
+	
+	controlPanel = document.getElementById("controlPanel");
+	hideControlPanel();
+}
+
+
+function tick(){
+	
+	var date = new Date();
+	displayPanel.textContent = date.toLocaleTimeString(); 
+	setTimeout(tick, 1000 - date.getMilliseconds());
+	
+}
+
+
+function initColorPicker(){
+	var colorPicker = document.getElementById("colorPicker");
+
+	var inputColor = document.createElement("input");
+	inputColor.setAttribute("type","color");
+	
+	if(inputColor.type=="color"){
+		inputColor.value = "black";
+		if(inputColor.value == "#000000"){
+			colorPicker.parentNode.replaceChild(inputColor, colorPicker);
+			inputColor.id = "colorPicker";
+			colorPicker = inputColor;
+		}
+	}
+	
+	colorPicker.addEventListener("change", setColor, false);
+}
+
+
+function setColor(event){
+
+	displayPanel.style.color = event.target.value;
+	saveData("color", event.target.value);
+}
+
+
+function saveData(name, data){
+	var storage = localStorage;
+	if(typeof storage == "undefined") return;
+	storage.setItem(name,data);
+}
+
+
+function setBackground(event){
+
+	var file = event.target.files[0];
+	window.URL = window.URL||window.webkitURL;
+	
+	var url=window.URL.createObjectURL(file);
+	document.body.style.backgroundImage="url('"+url+"')";
+	
+	var fileReader = new FileReader();
+	fileReader.onload = function(){saveData("image", this.result);};
+	fileReader.readAsDataURL(file);
+}
+
+
+function loadSettings(){
+	
+	var storage = localStorage;
+	if(typeof storage == "undefined") return;
+	
+	var textcolor = storage.getItem("color");
+	var image = storage.getItem("image");
+	
+	if(textcolor){
+		var colorPicker = document.getElementById("colorPicker");
+		displayPanel.style.color = textcolor;
+		colorPicker.value = textcolor;
+	}
+	
+	if(image){
+		document.body.style.backgroundImage = "url('"+image+"')";
+	}
+
+}
+
+
+function hideControlPanel(){
+	controlPanel.style.visibility="hidden";
+	
+	if(typeof document.ontouchmove == "undefined")
+		document.addEventListener("mousemove", showControlPanel);
+	else document.addEventListener("touchmove", showControlPanel);
+}
+
+
+function showControlPanel(){
+	controlPanel.style.visibility = "visible";
+	setTimeout(hideControlPanel,5000);
+	
+	if(typeof document.ontouchmove =="undefined")
+		document.addEventListener("mousemove", showControlPanel);
+	else document.addEventListener("touchmove", showControlPanel);
+}
